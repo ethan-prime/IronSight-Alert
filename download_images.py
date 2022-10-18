87e2ac7f5d8891ce218f3822c2a7803d67395617
Ethan <86028835+ethan-prime@users.noreply.github.com>
2022-10-17 22:06:45 -0500

    Delete download_images.py

--- download_images.py
+++
@@ -1,9 +0,0 @@
-from ast import keyword
-from simple_image_download import simple_image_download as simp
-
-response = simp.simple_image_download
-
-keywords = ['man with pistol', 'man with gun']
-
-for kw in keywords:
-    response().download(kw, 300)
