524bfacd16b7272e295bdde6b1c22c19dc9b39b4
Ethan <86028835+ethan-prime@users.noreply.github.com>
2022-10-17 22:06:50 -0500

    Delete fix_data.py

--- fix_data.py
+++
@@ -1,4 +0,0 @@
-import os
-
-for file in os.listdir(os.path.join('yolov7', 'data', 'val', 'labels')):
-    os.rename(os.path.join('yolov7', 'data', 'val', 'labels', file), os.path.join('yolov7', 'data', 'val', 'labels', file+'.txt'))
